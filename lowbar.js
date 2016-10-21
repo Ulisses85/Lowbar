f9138b09354b1c50b809894f9d01cbf375b5d4b2
tomcharlesosman <tomcharlesosman@gmail.com>
2016-10-21 13:06:45 +0100

    Add [_.first]

--- lowbar.js
+++ lowbar.js
@@ -3,9 +3,10 @@ var _ = {};
 _.indentity = function(a) {
   return a;
 };
-_.first = function() {
-
-}
+_.first = function(array, n) {
+  if (!n) return array[0];
+  else return array.slice(0, n);
+};
 
 if (typeof module !== 'undefined') {
   module.exports = _;
